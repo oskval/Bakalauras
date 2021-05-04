fa9d50b836cbab57dc1103cbd1138db2e4d4b376
oskval <oskaras.valentinavicius@outlook.com>
2021-05-04 18:34:13 +0300

    Styles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { NavComponent } from './nav/nav.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
